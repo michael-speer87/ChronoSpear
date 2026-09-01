67dd53efb48867cbe0dcf08e98e3af53280054ad
michael-speer87 <168745389+michael-speer87@users.noreply.github.com>
2026-09-01 13:09:28 -0500

    Expose Historical Occurrence types in CAM API

--- src/chronospear/cam/__init__.py
+++ src/chronospear/cam/__init__.py
@@ -2,8 +2,9 @@
 
 from chronospear.cam.associations import Association, AssociationCatalog
 from chronospear.cam.catalog import IdentityCatalog
-from chronospear.cam.identifiers import AssociationId, NodeId
+from chronospear.cam.identifiers import AssociationId, NodeId, OccurrenceId
 from chronospear.cam.identity import IdentityKind, IdentityNode
+from chronospear.cam.occurrences import HistoricalOccurrence, OccurrenceCatalog
 from chronospear.cam.relationships import RelationshipType, RelationshipVocabulary
 from chronospear.cam.time import ChronoStamp, SystemTime, WorldTime
 
@@ -12,10 +13,13 @@ __all__ = [
     "AssociationCatalog",
     "AssociationId",
     "ChronoStamp",
+    "HistoricalOccurrence",
     "IdentityCatalog",
     "IdentityKind",
     "IdentityNode",
     "NodeId",
+    "OccurrenceCatalog",
+    "OccurrenceId",
     "RelationshipType",
     "RelationshipVocabulary",
     "SystemTime",
